e89dff5b39690a65b759a80d79daa56a0fd1bc25
ankeshanand1 <47920054+ankeshanand1@users.noreply.github.com>
2021-04-10 01:53:16 +0530

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -3,38 +3,39 @@ import os
 import datetime
 from playsound import playsound
 
-def check(time):
-	if len(time) == 1:
-		if time[0] < 24 and alarm_time[0] >= 0:
+
+def check(atime):
+	if len(atime) == 1:
+		if atime[0] < 24 and atime[0] >= 0:
 			return True
-	if len(time) == 2: # [Hour:Minute] Format
-		if time[0] < 24 and alarm_time[0] >= 0 and \
-		   time[1] < 60 and alarm_time[1] >= 0:
+	if len(atime) == 2: # HH:MM
+		if atime[0] < 24 and atime[0] >= 0 and \
+		   atime[1] < 60 and atime[1] >= 0:
 			return True
-	elif len(time) == 3: # [Hour:Minute:Second] Format
-		if time[0] < 24 and alarm_time[0] >= 0 and \
-		   time[1] < 60 and alarm_time[1] >= 0 and \
-		   time[2] < 60 and alarm_time[2] >= 0:
+	elif len(atime) == 3: # HH:MM:SS
+		if atime[0] < 24 and atime[0] >= 0 and \
+		   atime[1] < 60 and atime[1] >= 0 and \
+		   atime[2] < 60 and atime[2] >= 0:
 			return True
 	else:
-    return False
+	    return False
 
 print("Set the Alarm Time (Format: HH:MM or HH:MM:SS) : ")
 
 while True:
-	alarm_input = input(">> ")
+	alarm_input=input(">> ")
 	try:
-		time = [int(n) for n in alarm_input.split(":")]
-		if check_alarm_input(alarm_time):
+		atime=[int(n) for n in alarm_input.split(":")]
+		if check(atime):
 			break
 		else:
 			raise ValueError
 	except ValueError:
-		print("ERROR: Enter time in HH:MM or HH:MM:SS format")
-    
+		print("ERROR: Enter in HH:MM or HH:MM:SS format")
+
 #convert to seconds
 seconds_hms = [3600, 60, 1]
-alarmseconds = sum([a*b for a,b in zip(seconds_hms[:len(alarm_time)], alarm_time)])
+alarmseconds = sum([a*b for a,b in zip(seconds_hms[:len(atime)], atime)])
 
 # Get the current time of day in seconds
 now = datetime.datetime.now()
@@ -43,9 +44,12 @@ currenttime = sum([a*b for a,b in zip(seconds_hms, [now.hour, now.minute, now.se
 # Calculate the number of seconds until alarm goes off
 diff = alarmseconds - currenttime
 
-#next day if alarm is negative
-if time_diff_seconds < 0:
-	time_diff_seconds += 86400
+#next day
+if diff < 0:
+	diff+= 86400
+
+# Display amount of time
+print("Alarm set to go off in:",datetime.timedelta(seconds=diff))
 
 time.sleep(diff)
 
