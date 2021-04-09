d0dc56f6179a5e60f76d1b03b5506d993ce25375
ankeshanand1 <47920054+ankeshanand1@users.noreply.github.com>
2021-04-10 01:08:57 +0530

    Create alarm.py

---
+++ alarm.py
@@ -0,0 +1,53 @@
+import time
+import os
+import datetime
+from playsound import playsound
+
+def check(alarm_time):
+	if len(alarm_time) == 1:
+		if alarm_time[0] < 24 and alarm_time[0] >= 0:
+			return True
+	if len(alarm_time) == 2: # [Hour:Minute] Format
+		if alarm_time[0] < 24 and alarm_time[0] >= 0 and \
+		   alarm_time[1] < 60 and alarm_time[1] >= 0:
+			return True
+	elif len(alarm_time) == 3: # [Hour:Minute:Second] Format
+		if alarm_time[0] < 24 and alarm_time[0] >= 0 and \
+		   alarm_time[1] < 60 and alarm_time[1] >= 0 and \
+		   alarm_time[2] < 60 and alarm_time[2] >= 0:
+			return True
+	else:
+    return False
+
+print("Set the Alarm Time (Format: HH:MM or HH:MM:SS) : ")
+
+while True:
+	alarm_input = input(">> ")
+	try:
+		alarm_time = [int(n) for n in alarm_input.split(":")]
+		if check_alarm_input(alarm_time):
+			break
+		else:
+			raise ValueError
+	except ValueError:
+		print("ERROR: Enter time in HH:MM or HH:MM:SS format")
+    
+#convert to seconds
+seconds_hms = [3600, 60, 1]
+alarmseconds = sum([a*b for a,b in zip(seconds_hms[:len(alarm_time)], alarm_time)])
+
+# Get the current time of day in seconds
+now = datetime.datetime.now()
+currenttime = sum([a*b for a,b in zip(seconds_hms, [now.hour, now.minute, now.second])])
+
+# Calculate the number of seconds until alarm goes off
+diff = alarmseconds - currenttime
+
+#next day if alarm is negative
+if time_diff_seconds < 0:
+	time_diff_seconds += 86400
+
+time.sleep(diff)
+
+print("Wake Up!")
+playsound('alarmm.mp3')
